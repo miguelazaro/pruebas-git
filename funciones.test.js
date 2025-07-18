96a77ae68c100dff3c8fb2e563a8b6148c507474
miguelazaro <a3522110262@alumno.uttehuacan.edu.mx>
2025-07-18 16:35:34 -0600

    test corregido  obtenerIniciales

--- funciones.test.js
+++ funciones.test.js
@@ -1,39 +1,22 @@
 const { suma, esPar, dividir, obtenerIniciales, formatearFecha } = require('./funciones');
 
-describe('Pruebas de las funciones', () => {
-    
-    test('suma: Devuelve la suma de dos números', () => {
-        expect(suma(2, 3)).toBe(5);
-        expect(suma(-2, 2)).toBe(0);
-        expect(suma(102, 62)).toBe(164);
-    });
+test('Suma de 2 + 3 es 5', () => {
+  expect(suma(2, 3)).toBe(5);
+});
 
-    test('esPar: Devuelve true si el número es par y false si es impar', () => {
-        expect(esPar(2)).toBe(true); 
-        expect(esPar(18)).toBe(true); 
-        expect(esPar(49)).toBe(false); 
-    });
+test('Número 4 es par', () => {
+  expect(esPar(4)).toBe(true);
+});
 
-    test('dividir: Devuelve "Error" si el divisor es 0', () => {
-        expect(dividir(10, 0)).toBe("Error");
-        expect(dividir(1000, 0)).toBe("Error");
-    });
+test('Dividir 6 entre 2 es 3', () => {
+  expect(dividir(6, 2)).toBe(3);
+});
 
-    test('dividir: Devuelve el resultado de la división', () => {
-        expect(dividir(10, 2)).toBe(5);
-        expect(dividir(9, 3)).toBe(3);
-    });
+test('Obtener iniciales de "Juan Pérez"', () => {
+  expect(obtenerIniciales("Juan Pérez")).toEqual("JP");
 
-    test('obtenerIniciales: Devuelve las iniciales del nombre completo', () => {
-        expect(obtenerIniciales("Miguel Lazaro")).toBe('YV');
-        expect(obtenerIniciales("Jesus Gonzales")).toBe('MH');
-        expect(obtenerIniciales("Ana")).toBe('A');
-    });
+});
 
-    test('formatearFecha: Devuelve la fecha en formato DD/MM/YYYY', () => {
-        expect(formatearFecha("2020-02-18")).toBe('18/02/2020');
-        expect(formatearFecha("01-01-2022")).toBe('01/01/2022');
-        expect(formatearFecha("2025-01-04")).toBe('04/01/2025');
-    });
-
+test('Formatear fecha "2024-07-18"', () => {
+  expect(formatearFecha("2024-07-18")).toBe("18/07/2024");
 });
